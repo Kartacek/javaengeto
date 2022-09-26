6db893d2fefaf3c114f05469c61ad78d4e28d802
kartacek <misa.vinor@seznam.cz>
2022-09-26 18:52:26 +0200

    uprava,dodelani_filosofu

--- philosophers2/src/com/engeto/philosophers2/Main.java
+++ philosophers2/src/com/engeto/philosophers2/Main.java
@@ -1,42 +1,45 @@
 package com.engeto.philosophers2;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 
+public class Main {
 
-        public class Main {
+    public static final int POCET_FILOZOFU = 10;
 
-            public static void main(String[] args) throws  Exception{
-                Philosopher philosopher[] = new Philosopher[10];
-                Object fork[] = new Object[10];
-
-                for(int i=0;i<philosopher.length;i++){
-                    fork[i] = new Object();
-                }
-
-                for(int i=0;i<philosopher.length;i++){
-                    Object leftFork = fork[i];
-                    Object rightFork = fork[(i+1)%5];
+    public static void main(String[] args) {
+        Lock[] vidlicky = new ReentrantLock[POCET_FILOZOFU];
 
 
-                    if(i == philosopher.length-1){
-                        philosopher[i] = new Philosopher(leftFork,rightFork);
-
-                    }else{
-                        philosopher[i] = new Philosopher(rightFork,leftFork);
-
-                    }
-                    Thread t = new Thread(philosopher[i] ," Philosopher "+(i+1));
-                    t.start();
-
-                }
-                System.out.println("ukol probehl");
-            }
+        for (int e = 0; e < POCET_FILOZOFU; e++) {
+            vidlicky[e] = new ReentrantLock();
 
         }
 
+
+        Philosopher[] filozofove = new Philosopher[POCET_FILOZOFU];
+
+        for (int e = 0; e < filozofove.length; e++) {
+            int filozofId = e + 1;
+            Lock pravaVidlicka = vidlicky[(e + 1) % vidlicky.length];
+            Lock levaVidlicka = vidlicky[e];
+
+
+
+
+            if (e == filozofove.length - 1) {
+                filozofove[e] = new Philosopher(filozofId, pravaVidlicka, levaVidlicka);
+            } else {
+                filozofove[e] = new Philosopher(filozofId, levaVidlicka, pravaVidlicka);
+            }
+
+            Thread vlakno = new Thread(filozofove[e], "Filozof : " + filozofId);
+            vlakno.start();
+        }
+    }
+}
 
 
--- philosophers2/src/com/engeto/philosophers2/Philosopher.java
+++ philosophers2/src/com/engeto/philosophers2/Philosopher.java
@@ -1,44 +1,94 @@
 package com.engeto.philosophers2;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
 
-public class Philosopher implements Runnable{
-    private Object leftFork;
-    private Object rightFork;
-    private int foodLeft = 10;
 
-    public Philosopher(Object leftFork,Object rightFork){
-        this.leftFork= leftFork;
-        this.rightFork= rightFork;
+
+
+
+
+public class Philosopher implements Runnable {
+    public static final int BOUND = 10;
+    Random nahodneCislo = new Random();
+    private int filId;
+    private Lock pVidlicka;
+    private Lock lVidlicka;
+
+
+
+
+
+
+
+
+    public Philosopher(int filId, Lock pVidlicka, Lock lVidlicka) {
+        this.filId = filId;
+        this.pVidlicka = pVidlicka;
+        this.lVidlicka = lVidlicka;
     }
 
-    public void doAction(String action) throws InterruptedException{
-        /*System.out.println(Thread.currentThread().getName()+" "+action);*/
-        Thread.sleep((int)(Math.random()*10000));
 
-    }
 
     @Override
+
+
+
+
+
     public void run() {
-        try{
-            while(foodLeft>0){
-                doAction(
-                        "Food left : "+this.foodLeft+"   : Thinking" );
-                synchronized (leftFork){
-                    doAction(
-                            "Food left : "+this.foodLeft+ " : Picked up left fork");
-                    synchronized (rightFork){
-                        doAction(
-                                "Food left: "+this.foodLeft+ " : Picked up right fork and started eating");
-                        doAction(
-                                "Food left : "+this.foodLeft+ " : Put down right fork");
-                        this.foodLeft-=1;
-                    }
-                    doAction(
-                            "Food left : "+this.foodLeft+ " : Put down left fork and start thinking");
-                }
+        try {
+
+            for (int i = 1; i <= 10000; i++) {
+                premysli();
+                zvedneLvidlicku();
+                zvednePvidlicku();
+                ji();
+                poloziVidlicku();
+
             }
-        }catch(InterruptedException e){
-            Thread.currentThread().interrupt();
-            return;
+        } catch (InterruptedException e) {
+            System.out.println("Vlákno f " + filId + " přerušeno.");
         }
+    }
+
+
+
+
+    public void premysli() throws InterruptedException {
+        System.out.println("Fil jménem: " + filId + " přemýšlí.");
+        System.out.flush();//vyprázdnění obsahu vyrovnávací paměti do výstupního proudu
+        Thread.sleep(nahodneCislo.nextInt(BOUND));
+    }
+
+
+    public void zvednePvidlicku() {
+        pVidlicka.lock();
+        System.out.println("Fil jménem: " + filId + " zvedne pvidličku.");
+        System.out.flush();
+    }
+
+
+    public void ji() throws InterruptedException {
+        System.out.println("Fil jménem: " + filId + " jí.");
+        System.out.flush();
+        Thread.sleep(nahodneCislo.nextInt(BOUND));
+    }
+
+
+    public void zvedneLvidlicku() {
+        lVidlicka.lock();
+        System.out.println("Fil jménem: " + filId + " zvedne lvidličku.");
+        System.out.flush();
+    }
+
+
+
+
+
+    public void poloziVidlicku() {
+        System.out.println("Fil jménem: " + filId + " položí vidličku");
+        System.out.flush();
+        lVidlicka.unlock();
+        pVidlicka.unlock();
     }
 }
